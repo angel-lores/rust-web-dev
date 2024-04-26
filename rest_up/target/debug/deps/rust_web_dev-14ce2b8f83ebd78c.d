7d6c509bfada6e5dafb36f7131e4faa18e802a1a
Angel Lores <angel-lores@outlook.com>
2024-04-26 15:38:44 -0700

    Delete rest_up/target directory

--- rest_up/target/debug/deps/rust_web_dev-14ce2b8f83ebd78c.d
+++
@@ -1,5 +0,0 @@
-/Users/lores/Documents/Projects/410P/rust-web-dev/target/debug/deps/librust_web_dev-14ce2b8f83ebd78c.rmeta: src/main.rs
-
-/Users/lores/Documents/Projects/410P/rust-web-dev/target/debug/deps/rust_web_dev-14ce2b8f83ebd78c.d: src/main.rs
-
-src/main.rs:
