c6a02215c9ede4690f95dadf0f87b9180d128796
Angel Lores <lores@pdx.edu>
2024-04-26 13:17:07 -0700

    rest up initial commit

---
+++ rest_up/target/debug/deps/rust_web_dev-14ce2b8f83ebd78c.d
@@ -0,0 +1,5 @@
+/Users/lores/Documents/Projects/410P/rust-web-dev/target/debug/deps/librust_web_dev-14ce2b8f83ebd78c.rmeta: src/main.rs
+
+/Users/lores/Documents/Projects/410P/rust-web-dev/target/debug/deps/rust_web_dev-14ce2b8f83ebd78c.d: src/main.rs
+
+src/main.rs:
---
+++ rest_up/target/debug/deps/rust_web_dev-9ca7162a0f814821.d
@@ -0,0 +1,5 @@
+/Users/lores/Documents/Projects/410P/rust-web-dev/target/debug/deps/librust_web_dev-9ca7162a0f814821.rmeta: src/main.rs
+
+/Users/lores/Documents/Projects/410P/rust-web-dev/target/debug/deps/rust_web_dev-9ca7162a0f814821.d: src/main.rs
+
+src/main.rs:
